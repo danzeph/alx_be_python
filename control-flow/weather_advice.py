f0c2204a43d1f4e878ddcf5526d918eafc40555c
danzeph <danzeph00@gmail.com>
2025-11-11 14:21:51 +0000

    Correct clothing advice and print condition
    Fixed typo in clothing variable and added condition to print advice.

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,15 +1,17 @@
 weather = input("What's the weather like today? (sunny/rainy/cold): ")
 
+clothing = None
 if(weather == "sunny"):
     clothing = "Wear a t-shirt and sunglasses."
 elif (weather == "rainy"):
     clothig = "Dont forget your umbrella and a scarf."
 elif (weather == "cold"):
-    clothing = "Make sure to wear a warm coat and a scarf"
+    clothing = "Make sure to wear a warm coat and a scarf."
 else:
     print("Sorry, I don't have recommendations for this weather.")
 
-print(cothing)
+if clothing is not None:
+    print(clothing)
 
    
 
