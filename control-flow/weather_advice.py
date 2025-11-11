a24394ffaf035ec90f686369d187b77be9b83d7a
danzeph <danzeph00@gmail.com>
2025-11-11 14:25:58 +0000

    Update weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,4 +1,4 @@
-weather = input("What's the weather like today? (sunny/rainy/cold): ")
+weather = input("What's the weather like today? (sunny/rainy/cold): ").tolower()
 
 clothing = None
 if(weather == "sunny"):
