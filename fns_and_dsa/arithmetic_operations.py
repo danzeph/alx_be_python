2efa1f11f6e748da2b3f658f31f44c8b887a6f09
danzeph <danzeph00@gmail.com>
2025-12-02 16:11:18 +0000

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,18 +1,18 @@
 def perform_operation(num1, num2, operation):
-  resullt = None
-  match operation:
-    case "add":
-      result = num1 + num2
-    case "subtract":
-      result = num1 - num2
-    case "multiply":
-      result = num1 * num2
-    case "divide":
-      if (num2 > 0):
-        result = num1 / num2
-      else:
-        result = "Cannot divide by Zero"
-    case _:
-      result = "Please Enter a Valid operation(add, subtract, multiply, divide,"
+    result = None
     
-  return result
+    if operation == "add":
+        result = num1 + num2
+    elif operation == "subtract":
+        result = num1 - num2
+    elif operation == "multiply":
+        result = num1 * num2
+    elif operation == "divide":
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            result = "Cannot divide by Zero"
+    else:
+        result = "Please Enter a Valid operation (add, subtract, multiply, divide)"
+    
+    return result
